eedd4125c42588de96673438306c1ae61536d9a4
Kenny Chen <kenny@brobridge.com>
2023-06-28 19:59:32 +0800

    1. update atomic flow.json 2. set atomic service type to nodePort 3. fix sql file path in insert_20 script

--- assets/mssql/insert_source_20.sh
+++ assets/mssql/insert_source_20.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/mssql-tools/bin/sqlcmd -U sa -P ${SA_PASSWORD} -e -i /assets/mssql/insert_source.sql
+/opt/mssql-tools/bin/sqlcmd -U sa -P ${SA_PASSWORD} -e -i /assets/assets/mssql/insert_source.sql
